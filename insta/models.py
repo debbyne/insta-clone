7bbdd7f23ab28253c83ef3616301d91047c8b93d
debbyne <kinyadebbyne@gmail.com>
2022-04-01 09:56:28 +0300

    add model class

--- insta/models.py
+++ insta/models.py
@@ -1,3 +1,12 @@
 from django.db import models
-
+from email.mime import image
+from unicodedata import category, name
+from django.db import models
 # Create your models here.
+class Images(models.Model):
+    image = models.ImageField(upload_to = 'photos/',default="",null=True)
+    name = models.CharField(max_length=50,null=True)
+    caption = models.CharField(max_length=100,null=True)
+    # image = CloudinaryField('image')
+    # location = models.ForeignKey(Location,on_delete=models.RESTRICT,)
+    # category = models.ForeignKey(Category, on_delete=models.RESTRICT,)
--- insta/views.py
+++ insta/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http  import HttpResponse
 
 # Create your views here.
+images = Images.get_images()
+    title= 'Thee Dev Gallery'
+    return render(request, 'all-photos/pictures.html', {"images":images, "title":title})
