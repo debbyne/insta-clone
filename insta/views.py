32c7d53892ff56afdeb6a0fec0a6508298328c14
debbyne <kinyadebbyne@gmail.com>
2022-04-01 10:06:48 +0300

    update folder structure

--- insta/views.py
+++ insta/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.http  import HttpResponse
+from .models import Images
 
 # Create your views here.
-images = Images.get_images()
-    title= 'Thee Dev Gallery'
+def index(request):
+    images = Images.get_images()
+    title= 'Photo Display'
     return render(request, 'all-photos/pictures.html', {"images":images, "title":title})
