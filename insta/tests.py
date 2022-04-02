fe4630e70c194bcc9218ec2e5877280dbc5d48d8
debbyne <kinyadebbyne@gmail.com>
2022-04-02 08:48:17 +0300

    add test to images class

--- insta/tests.py
+++ insta/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from .models import Images
 
 # Create your tests here.
+class ImageTestClass(TestCase):
+    def setUp(self):
+       pass
+    def test_instance(self):
+        self.assertTrue(isinstance(self.test_image,Images))
+     
+    def test_save_image(self):
+        self.image.save_image()
+        self.assertTrue(len(Images.objects.all()) == 1)
+    def test_delete_image(self):
+        self.test_image.delete_image()
+        image = Images.objects.all()
+        self.assertTrue(len(Images.objects.all()) ==1)
--- instaapp/settings.py
+++ instaapp/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'insta',
+    'bootstrap3'
 ]
 
 MIDDLEWARE = [
