e09313cc6865209aa301081b36c946ae3da0b04e
debbyne <kinyadebbyne@gmail.com>
2022-04-03 11:39:44 +0300

    add models to admin

--- insta/admin.py
+++ insta/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Post,Comments,Profile,Follow
 
 # Register your models here.
+admin.site.register(Post)
+admin.site.register(Profile)
+admin.site.register(Comments)
+admin.site.register(Follow)
