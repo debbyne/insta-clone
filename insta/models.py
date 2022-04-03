5c278d47eac4c14cb056e11eea4e964edc9b49ff
debbyne <kinyadebbyne@gmail.com>
2022-04-03 11:25:48 +0300

    update models

--- insta/models.py
+++ insta/models.py
@@ -1,12 +1,46 @@
 from django.db import models
 from email.mime import image
-from unicodedata import category, name
+from django.contrib.auth.models import User
+from unicodedata import name
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
 # Create your models here.
 
 class Profile(models.Model):
-     name=models.CharField(max_length=50)
-     bio=models.TextField(max_length=500,blank=True)
+     name=models.CharField(max_length=50, blank=True)
+     bio=models.TextField(max_length=400, default="My Bio",blank=True)
+     profile_picture=models.ImageField(upload_to = 'photos/',default="",null=True)
+     user=models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+     location = models.CharField(max_length=50, blank=True)
+
+
+
+     def __str__(self):
+        return f'{self.user.username} Profile'
+
+     @receiver(post_save, sender=User)
+     def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
+
+     @receiver(post_save, sender=User)
+     def save_user_profile(sender, instance, **kwargs):
+        instance.profile.save()
+
+     def save_profile(self):
+        self.user
+
+     def delete_profile(self):
+        self.delete()
+
+     @classmethod
+     def search_profile(cls, name):
+        return cls.objects.filter(user__username__icontains=name).all()
+
+     def __str__(self):
+        return f'{self.user.username} Profile'
 
 
 class Post(models.Model):
@@ -14,21 +48,51 @@ class Post(models.Model):
     name = models.CharField(max_length=50,null=True)
     caption = models.CharField(max_length=100,null=True)
     # image = CloudinaryField('image')
-    
-    
-    def save_post(self):
+    likes = models.ManyToManyField(User, related_name='likes', blank=True, )
+    user = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='posts', null=True)
+    time_created=models.DateTimeField(auto_now_add=True, null=True)
+
+    class Meta:
+        ordering = ["-pk"]
+
+    def get_absolute_url(self):
+        return f"/post/{self.id}"
+
+    @property
+    def get_all_comments(self):
+        return self.comments.all()
+
+    def save_image(self):
         self.save()
 
-    def __str__(self):
-        return self.name
-
+    def total_likes(self):
+        return self.likes.count()
     
-    def delete_post(self):
+    def delete_image(self):
         self.delete()
+
+    def __str__(self):
+        return f'{self.user.username} Post'
+
     @classmethod
-    def update_post(cls ,id ,image):
+    def update_image(cls ,id ,image):
         return cls.objects.filter(id = id).update(image = image)
-    @classmethod
-    def get_images(cls):
-        images = cls.objects.all()
-        return images
+
+class Comments(models.Model):
+    comment = models.TextField()
+    user = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='comments')
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    time_created = models.DateTimeField(auto_now_add=True, null=True)
+
+    class Meta:
+        ordering = ["-pk"]
+
+    def __str__(self):
+        return f'{self.user.name} Post'
+
+class Follow(models.Model):
+    followed = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='followers')
+    follower = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='following')
+
+    def __str__(self):
+        return f'{self.follower} Follow'  
