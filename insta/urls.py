c3220287cc18c4a648f35eb8eeae91547039e0f1
debbyne <kinyadebbyne@gmail.com>
2022-04-03 12:28:53 +0300

    add search function

--- insta/urls.py
+++ insta/urls.py
@@ -5,7 +5,7 @@ from django.conf.urls.static import static
 
 urlpatterns=[
     path('',views.index,name = 'index'),
-    # path('search/', views.search_results, name='search_results'),
+    path('search/', views.search_results, name='search_results'),
 ]   
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- insta/views.py
+++ insta/views.py
@@ -1,10 +1,25 @@
 from django.shortcuts import render
 from django.http  import HttpResponse
-from .models import Post
+from .models import Post,Profile
+from django.contrib.auth.decorators import login_required
 
 
 # Create your views here.
 def index(request):
     post = Post.objects.all()
     return render(request, 'index.html')
+
+@login_required(login_url='/accounts/login')
+def search_results(request):
+
+    if 'search_username' in request.GET and request.GET["search_username"]:
+        search_term = request.GET.get("search_username")
+        searched_username = Profile.search_profile(search_term)
+        message = f"{search_term}"
+
+        return render(request, 'search.html',{"message":message, 'profile':searched_username})
+
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'search.html',{"message":message})
 
